d806b9a941a5a258a9d5dcde9e9ca0a576c576b0
juanyep <yepezjuan6@gmail.com>
2026-04-03 16:56:33 -0700

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p, q) {
+
+        //basically search recursivley on 2 trees
+
+        //base case maybe not bc we are returning false
+        // but i rem. that recursion should return
+        if (!p && !q) return true
+
+        if(!p || !q || p.val !== q.val) return false
+
+
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right)
+        //bottom-up or top-down would both work here , I THINK?!???
+
+    }
+}
