60ab912ba365945dd9a09b7beb38715a564f20cb
juanyep <yepezjuan6@gmail.com>
2026-07-27 11:40:51 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) { 
+        //want to see if its a letter first
+        // clean it up using ascii values
+        //then combine it back
+        // while loop L < R 
+
+        let newS = s.toLowerCase().split('').filter(c => {
+            let code  = c.charCodeAt(0);
+            return (code >= 97 && code <= 122) || (code >= 48 && code <= 57)
+        })
+
+
+        let left = 0 
+        let right = newS.length - 1
+
+        while( left < right){
+            if(newS[left] != newS[right]) return false
+            left++
+            right--
+        }
+
+        return true
+    }
+}
